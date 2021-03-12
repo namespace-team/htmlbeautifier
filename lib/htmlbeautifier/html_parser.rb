642dbf479c0af1a14eeed2df88d3928bb3e6dc08
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-03-12 12:08:12 +0545

    Fixing forever loop (#1)
    * has_rdoc is deprecated with no replacement
    
    * update emit  function to fix error
    
    * adding span tag and updating MAPPINGS
    
    * fixing the rubocop

--- lib/htmlbeautifier/html_parser.rb
+++ lib/htmlbeautifier/html_parser.rb
@@ -11,7 +11,8 @@ module HtmlBeautifier
       address | article | aside | audio | blockquote | canvas | dd | dir | div |
       dl | dt | fieldset | figcaption | figure | footer | form | h1 | h2 | h3 |
       h4 | h5 | h6 | header | hr | li | menu | noframes | noscript | ol | p |
-      pre | section | table | tbody | td | tfoot | th | thead | tr | ul | video
+      pre | section | span | table | tbody | td | tfoot | th | thead | tr | ul |
+      video
     )}mix
 
     MAPPINGS = [
@@ -35,16 +36,16 @@ module HtmlBeautifier
        :preformatted_block],
       [%r{<#{HTML_VOID_ELEMENTS}(?: #{ELEMENT_CONTENT})?/?>}om,
        :standalone_element],
-      [%r{<\w+(?: #{ELEMENT_CONTENT})?/>}om,
-       :standalone_element],
       [%r{</#{HTML_BLOCK_ELEMENTS}>}om,
        :close_block_element],
       [%r{<#{HTML_BLOCK_ELEMENTS}(?: #{ELEMENT_CONTENT})?>}om,
        :open_block_element],
       [%r{</#{ELEMENT_CONTENT}>}om,
        :close_element],
-      [%r{<#{ELEMENT_CONTENT}>}om,
+      [%r{<#{ELEMENT_CONTENT}[^/]>}om,
        :open_element],
+      [%r{<\w+(?: #{ELEMENT_CONTENT})?/>}om,
+       :standalone_element],
       [%r{(\s*\r?\n\s*)+}om,
        :new_lines],
       [%r{[^<\n]+},
